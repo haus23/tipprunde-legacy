36edb29bb95d037e5895d7b990375dc3800bcecd
Micha Buchholz <micha@haus23.net>
2022-09-10 15:22:37 +0200

    Introduce first idea of app routes.

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,9 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Dashboard from './app/dashboard';
+
 import Layout from './app/layout';
+import Dashboard from './app/dashboard';
+import Login from './app/login';
+import Error from './app/error';
 
 export default function App() {
   return (
@@ -8,6 +11,8 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Error />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,39 @@
-import { Outlet } from 'react-router-dom';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { useAuth } from '@/hooks/use-auth';
+
 import Logo from '@/components/logo';
 
 export default function Layout() {
+  const { isAuthenticated } = useAuth();
+  const location = useLocation();
+
+  if (!isAuthenticated && location.pathname !== '/login') {
+    return <Navigate to="/login" replace state={{ from: location }} />;
+  }
+
   return (
     <div>
-      {/* Drawer sidebar for mobile */}
-      <div className="hidden"></div>
-      {/* Static sidebar for desktop */}
-      <div className="hidden"></div>
+      {isAuthenticated && (
+        <>
+          <div className="hidden">{/* Drawer sidebar for mobile */}</div>
+          <div className="hidden">{/* Static sidebar for desktop */}</div>
+        </>
+      )}
       {/* Content with header */}
       <div className="flex flex-col">
-        {/* Header */}
-        <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
-          {/* Toggle Button */}
+        {/* Toggle menu button or login header */}
+        {isAuthenticated ? (
           <button className="hidden"></button>
-          {/* Navbar */}
-          <div className="flex flex-1 justify-between px-4">
-            <div className="flex items-center gap-x-2">
-              <Logo className="h-10" />
-              <h1 className="text-2xl font-semibold">runde.tips</h1>
+        ) : (
+          <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow">
+            <div className="flex flex-1 justify-between px-4">
+              <div className="flex items-center gap-x-2">
+                <Logo className="h-10" />
+                <h1 className="text-2xl font-semibold">runde.tips</h1>
+              </div>
             </div>
           </div>
-        </div>
+        )}
         {/* Content */}
         <main className="flex-1">
           <div className="mx-auto max-w-7xl px-4 sm:px-6 md:px-8">
