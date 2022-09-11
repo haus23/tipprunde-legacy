63f5f2c754a35988ed455cc669bca477d33b0e72
Micha Buchholz <micha@haus23.net>
2022-09-11 13:47:36 +0200

    Extract full fledged text field component.

--- src/app/login.tsx
+++ src/app/login.tsx
@@ -4,6 +4,7 @@ import { useNavigate } from 'react-router-dom';
 
 import { useAuth } from '@/hooks/use-auth';
 import Button from '@/components/button';
+import TextField from '@/components/form/text-field';
 
 type LoginFormType = {
   email: string;
@@ -35,45 +36,25 @@ export default function Login() {
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
-          <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Email
-              </label>
-              <div className="mt-1">
-                <input
-                  {...register('email')}
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
-            <div>
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium text-gray-700"
-              >
-                Passwort
-              </label>
-              <div className="mt-1">
-                <input
-                  {...register('password')}
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full appearance-none rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
-                />
-              </div>
-            </div>
+          <form
+            className="space-y-6"
+            onSubmit={handleSubmit(onSubmit)}
+            noValidate
+          >
+            <TextField
+              label="Email"
+              required
+              type="email"
+              error={errors.email?.message}
+              {...register('email', { required: true })}
+            />
+            <TextField
+              label="Passwort"
+              required
+              type="password"
+              error={errors.password?.message}
+              {...register('password', { required: true })}
+            />
             <div>
               <Button>Anmelden</Button>
             </div>
