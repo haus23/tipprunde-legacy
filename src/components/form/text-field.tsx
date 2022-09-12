814467a283e1ec7724dce630f4cede487615a2d3
Micha Buchholz <micha@haus23.net>
2022-09-12 21:58:52 +0200

    Show errored input in red as well

--- src/components/form/text-field.tsx
+++ src/components/form/text-field.tsx
@@ -40,7 +40,7 @@ function TextField(
           className={classNames(
             'block w-full appearance-none rounded-md border  px-3 py-2 placeholder-gray-400 shadow-sm focus:outline-none sm:text-sm',
             hasError
-              ? 'border-red-300 focus:border-red-500 focus:ring-red-500'
+              ? 'text-red-500 border-red-300 focus:border-red-500 focus:ring-red-500'
               : 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500'
           )}
         />
