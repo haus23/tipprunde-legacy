aa28e151b2f9c6537c2373492645571d338e6bfb
Micha Buchholz <micha@haus23.net>
2022-09-12 21:15:48 +0200

    Implement creating entities with sequence id.

--- src/hooks/domain/use-players.ts
+++ src/hooks/domain/use-players.ts
@@ -1,8 +1,13 @@
+import { createWithSequence } from '@/firebase/db/repository/create-entity';
+import { Player } from '@/model/domain/player';
 import { playersState } from '@/states/domain/players-state';
 import { useRecoilValue } from 'recoil';
 
 export function usePlayers() {
   const players = useRecoilValue(playersState);
 
-  return { players };
+  const createPlayer = (player: Player) =>
+    createWithSequence<Player>('players', 'player', player);
+
+  return { players, createPlayer };
 }
