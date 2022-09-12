a2bf0a7db2ced5acf611c8eba1702faf13816acc
Micha Buchholz <micha@haus23.net>
2022-09-12 14:31:23 +0200

    Implement a typed and synced firestore collection wrapper.

---
+++ src/firebase/db/collection.ts
@@ -0,0 +1,28 @@
+import {
+  QueryConstraint,
+  collection as firestoreCollection,
+  query,
+  onSnapshot,
+} from 'firebase/firestore';
+import { db } from '@/firebase/db';
+
+import { BaseModel } from './base-model';
+import { converter } from './converter';
+
+export const collection = <T extends BaseModel>(
+  path: string,
+  ...constraints: QueryConstraint[]
+) => {
+  const q = query(firestoreCollection(db, path), ...constraints).withConverter(
+    converter<T>()
+  );
+
+  return {
+    subscribe: (subscriber: (entities: T[]) => void) => {
+      const unsubscribe = onSnapshot(q, (snapshot) => {
+        subscriber(snapshot.docs.map((d) => d.data()));
+      });
+      return unsubscribe;
+    },
+  };
+};
