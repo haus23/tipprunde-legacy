1633ace097ffbabae88070abec0c4bf081addb21
Micha Buchholz <micha@haus23.net>
2022-09-15 14:24:26 +0200

    Format: remove space.

--- src/components/form/text-field.tsx
+++ src/components/form/text-field.tsx
@@ -39,7 +39,7 @@ function TextField(
           autoComplete="off"
           {...props}
           className={classNames(
-            'block w-full appearance-none rounded-md border  px-3 py-2 placeholder-gray-400 shadow-sm focus:outline-none sm:text-sm',
+            'block w-full appearance-none rounded-md border px-3 py-2 placeholder-gray-400 shadow-sm focus:outline-none sm:text-sm',
             hasError
               ? 'text-red-500 border-red-300 focus:border-red-500 focus:ring-red-500'
               : 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500'
