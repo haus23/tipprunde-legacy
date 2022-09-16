18afaa60e0dab215fd54e160193bb63c19e62598
Micha Buchholz <micha@haus23.net>
2022-09-16 11:16:13 +0200

    Start implementing the profile view.

--- src/app/profile/index.tsx
+++ src/app/profile/index.tsx
@@ -1,3 +1,82 @@
+import Button from '@/components/button';
+import SimpleSelectField from '@/components/form/simple-select-field';
+import TextField from '@/components/form/text-field';
+import { useProfile } from '@/hooks/use-profile';
+import { Profile, ROLES } from '@/model/profile';
+import { notify } from '@/utils/notify';
+import { useForm, Controller } from 'react-hook-form';
+
 export default function ProfileView() {
-  return <h2 className="text-2xl font-semibold">Profil</h2>;
+  /* TODO: disable or remove role editing here! */
+
+  const { profile, updateDisplayName, updateRole } = useProfile();
+  const {
+    control,
+    handleSubmit,
+    register,
+    formState: { isDirty, dirtyFields, errors },
+  } = useForm<Profile>({ defaultValues: profile });
+
+  async function saveProfile(profile: Profile) {
+    if (dirtyFields.displayName) {
+      notify(
+        updateDisplayName(profile.displayName || ''),
+        'Dein Name wurde geändert.'
+      );
+    }
+    if (dirtyFields.role) {
+      notify(updateRole(profile.role), 'Rolle wurde geändert.');
+    }
+  }
+
+  return (
+    <div className="space-y-4">
+      <h2 className="text-2xl font-semibold">Profil</h2>
+      <div className="mt-5">
+        <div className="shadow rounded-md bg-white">
+          <form onSubmit={handleSubmit(saveProfile)} noValidate>
+            <div className="space-y-4 p-4">
+              <TextField
+                disabled
+                required
+                label="Email"
+                {...register('email')}
+              />
+              <TextField
+                label="Name"
+                minLength={3}
+                placeholder="Darf auch leer bleiben"
+                error={errors.displayName?.message}
+                {...register('displayName', {
+                  validate: {
+                    minChars: (name) =>
+                      !name ||
+                      name.trim().length >= 3 ||
+                      'Drei Zeichen sollten es schon sein ;-)',
+                  },
+                })}
+              />
+              <Controller
+                control={control}
+                name="role"
+                render={({ field: { value, onChange } }) => (
+                  <SimpleSelectField
+                    value={value}
+                    onChange={onChange}
+                    label="Rolle"
+                    options={ROLES}
+                  />
+                )}
+              />
+            </div>
+            <div className="bg-gray-50 px-4 py-3 text-right sm:px-6 space-x-4">
+              <Button disabled={!isDirty} primary type="submit">
+                Speichern
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/hooks/use-profile.ts
+++ src/hooks/use-profile.ts
@@ -1,10 +1,25 @@
-import { useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue } from 'recoil';
 import { authState } from '@/states/auth-state';
 import { User } from '@/model/user';
 import { profileState } from '@/states/profile-state';
+import { updateProfile } from 'firebase/auth';
+import { auth } from '@/firebase/auth';
+import { Role } from '@/model/profile';
+import { patchEntity } from '@/firebase/db/repository/patch-entity';
 
 export function useProfile() {
   const user = useRecoilValue(authState) as User;
-  const profile = useRecoilValue(profileState(user));
-  return { profile };
+  const [profile, setProfile] = useRecoilState(profileState(user));
+
+  const updateDisplayName = async (displayName: string) => {
+    await updateProfile(auth.currentUser!, { displayName });
+    setProfile({ ...profile, displayName });
+  };
+
+  const updateRole = async (role: Role) => {
+    await patchEntity('users', profile, { role });
+    setProfile({ ...profile, role });
+  };
+
+  return { profile, updateDisplayName, updateRole };
 }
