f0de14c86892bee877a252e7239727ae0ec9cbab
Micha Buchholz <micha@haus23.net>
2022-09-26 20:29:32 +0200

    Tweak git for windows (change case of file).

--- apps/foh/src/main.tsx
+++ apps/foh/src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import './app.css';
-import App from '@/app';
+import App from '@/app-root';
 
 const rootContainer = document.getElementById('root') as HTMLElement;
 createRoot(rootContainer).render(
