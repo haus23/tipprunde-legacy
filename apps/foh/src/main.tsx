956dc660e4f45a5be705c76442591696dc0e26ca
Micha Buchholz <micha@haus23.net>
2022-09-26 19:44:59 +0200

    Align project and root files.

--- apps/foh/src/main.tsx
+++ apps/foh/src/main.tsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import './app.css';
+import App from '@/app';
+
+const rootContainer = document.getElementById('root') as HTMLElement;
+createRoot(rootContainer).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
