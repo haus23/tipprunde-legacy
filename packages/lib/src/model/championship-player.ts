d60897850b9d9b6d50a1464c7f9e37ef1b049536
Micha Buchholz <micha@haus23.net>
2022-10-06 23:55:19 +0200

    Create new model: championship-player

---
+++ packages/lib/src/model/championship-player.ts
@@ -0,0 +1,10 @@
+import { BaseModel } from './base/model';
+
+export type ChampionshipPlayer = BaseModel & {
+  playerId: string;
+  nr: number;
+  points: number;
+  extraPoints: number;
+  totalPoints: number;
+  rank: number;
+};
