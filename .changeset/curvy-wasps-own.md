ade137dedb7285c06c884e90340174f3c4926605
Micha Buchholz <micha@haus23.net>
2022-10-06 18:31:37 +0200

    Add card and toggle-field components

---
+++ .changeset/curvy-wasps-own.md
@@ -0,0 +1,5 @@
+---
+'ui': patch
+---
+
+Add card and toggle-field components.
--- packages/ui/src/index.tsx
+++ packages/ui/src/index.tsx
@@ -1,6 +1,9 @@
 export * from './components/brand/app-title';
 export * from './components/brand/splash-screen';
 
+export * from './components/container/card';
+
 export * from './components/form/select-field';
+export * from './components/form/toggle-field';
 
 export * from './utils/class-names';
