c80f48bc80edc4ecc595c8ade243513acd685bfb
Micha Buchholz <micha@haus23.net>
2022-10-21 21:03:26 +0200

    Add simple date field.

---
+++ .changeset/rotten-pugs-tan.md
@@ -0,0 +1,5 @@
+---
+'ui': patch
+---
+
+Add simple date field.
---
+++ packages/ui/src/components/form/date-field.tsx
@@ -0,0 +1,47 @@
+import { useId } from 'react';
+import {
+  Control,
+  FieldPathByValue,
+  FieldValues,
+  useController,
+} from 'react-hook-form';
+
+type DateFieldProps<
+  T extends FieldValues,
+  TPath extends FieldPathByValue<T, string>
+> = {
+  label: string;
+  control: Control<T>;
+  name: TPath;
+};
+
+export function DateField<
+  T extends FieldValues,
+  TPath extends FieldPathByValue<T, string>
+>({ label, control, name }: DateFieldProps<T, TPath>) {
+  const id = `${useId()}-${name}`;
+
+  const {
+    field: { value, onChange },
+  } = useController({
+    control,
+    name,
+  });
+
+  return (
+    <div>
+      <label htmlFor={id} className="block text-sm font-medium text-gray-700">
+        {label}
+      </label>
+      <div className="relative mt-1">
+        <input
+          id={id}
+          type="date"
+          className="form-input block w-full rounded-md shadow-sm sm:text-sm border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
+          value={value ?? ''}
+          onChange={onChange}
+        />
+      </div>
+    </div>
+  );
+}
