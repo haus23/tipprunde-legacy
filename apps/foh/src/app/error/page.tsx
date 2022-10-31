51791092a802c3cc986341edc9a6d0d732112264
Micha Buchholz <micha@haus23.net>
2022-10-31 22:27:48 +0100

    Remove the countdown gimmick.

--- apps/foh/src/app/error/page.tsx
+++ apps/foh/src/app/error/page.tsx
@@ -1,26 +1,8 @@
-import { useCallback, useEffect, useReducer } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button } from 'ui';
 
 export default function ErrorPage() {
-  const [countdown, signal] = useReducer((count) => count - 1, 5);
-
   const navigate = useNavigate();
-
-  const goHome = useCallback(() => {
-    navigate('/');
-  }, [navigate]);
-
-  useEffect(() => {
-    const timer = setInterval(() => signal(), 1000);
-    return () => clearInterval(timer);
-  }, []);
-
-  useEffect(() => {
-    if (countdown === 0) {
-      goHome();
-    }
-  }, [countdown, goHome]);
 
   return (
     <div className="h-full flex flex-col justify-center mx-auto max-w-3xl px-4 sm:px-6 lg:px-8">
@@ -32,10 +14,9 @@ export default function ErrorPage() {
             Hier lief was schief. Sag uns Bescheid und wir versuchen den Fehler
             zu finden.
           </p>
-          <p>Oder hast du aus Versehen ein falsches Turnier eingegeben?</p>
         </div>
         <div className="text-center">
-          <Button onClick={goHome}>Zur Startseite ({countdown})</Button>
+          <Button onClick={() => navigate('/')}>Zur Startseite</Button>
         </div>
       </div>
     </div>
