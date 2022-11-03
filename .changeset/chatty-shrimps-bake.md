1a55106b93ce3878bba4c8994b0930d341947238
Micha Buchholz <micha@haus23.net>
2022-11-03 13:46:06 +0100

    Add hidden refactor route.

---
+++ .changeset/chatty-shrimps-bake.md
@@ -0,0 +1,5 @@
+---
+'backyard': patch
+---
+
+Add hidden refactor route.
---
+++ apps/backyard/src/app/refactor/index.tsx
@@ -0,0 +1,3 @@
+export default function RefactorView() {
+  return <h2 className="text-2xl font-semibold">Refactor</h2>;
+}
