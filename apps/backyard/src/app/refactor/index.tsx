56482973c67f22b41111bfdb8c49a037aa78b247
Micha Buchholz <micha@haus23.net>
2022-11-03 14:13:40 +0100

    Refactoring round ids.

--- apps/backyard/src/app/refactor/index.tsx
+++ apps/backyard/src/app/refactor/index.tsx
@@ -1,3 +1,52 @@
+import { useCurrentChampionship } from '@/hooks/current-data/use-current-championship';
+import { useMatches } from '@/hooks/current-data/use-matches';
+import { useRounds } from '@/hooks/current-data/use-rounds';
+import { deleteEntity, Match } from 'lib';
+import { Button } from 'ui';
+
 export default function RefactorView() {
-  return <h2 className="text-2xl font-semibold">Refactor</h2>;
+  const { currentChampionship } = useCurrentChampionship();
+  const { rounds } = useRounds();
+  const { matches, updateMatch } = useMatches();
+  const refactorRoundIdsDone = !rounds.some((r) => r.id.startsWith('runde-'));
+
+  function refactorRoundIds() {
+    rounds
+      .filter((r) => r.id.startsWith('runde-'))
+      .forEach(async (oldRound) => {
+        // Find corresponding new round
+        const newRound = rounds.find(
+          (r) => r.nr === oldRound.nr && r !== oldRound
+        );
+        if (newRound) {
+          matches
+            .filter((m) => m.roundId === oldRound.id)
+            .forEach(async (m) => {
+              const updatedMatch: Match = { ...m, roundId: newRound.id };
+              await updateMatch(updatedMatch);
+              console.log('Updated Match', updatedMatch.nr);
+            });
+          await deleteEntity(
+            `championships/${currentChampionship?.id}/rounds`,
+            oldRound.id
+          );
+          console.log('Deleted old round', oldRound.id);
+        }
+      });
+  }
+
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold">Refactor</h2>
+      <div className="mt-8 flex flex-wrap gap-8">
+        <Button
+          onClick={refactorRoundIds}
+          primary
+          disabled={refactorRoundIdsDone}
+        >
+          Refactor Round Ids
+        </Button>
+      </div>
+    </div>
+  );
 }
