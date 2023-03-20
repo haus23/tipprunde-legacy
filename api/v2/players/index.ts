0aa68b66283bd7125f146aa74a384002619e2307
Micha Buchholz <micha@haus23.net>
2023-03-20 17:52:19 +0100

    refactor: create business logic query helper

--- api/v2/players/index.ts
+++ api/v2/players/index.ts
@@ -1,18 +1,3 @@
-import { Player } from '@haus23/tipprunde-types';
-import { db, modelConverter } from '~/lib/firebase';
-import { cachedQuery } from '~/lib/util/cached-query';
+import { getPlayers } from '~/lib/query/entity/get-players';
 
-export default eventHandler(() => {
-  const getPlayers = cachedQuery(
-    async function () {
-      console.log('Querying');
-      const snapshot = await db.collection('players').withConverter(modelConverter<Player>()).get();
-      return snapshot.docs.map((doc) => doc.data());
-    },
-    {
-      name: 'entities',
-      getKey: () => 'players',
-    }
-  );
-  return getPlayers();
-});
+export default eventHandler(getPlayers);
