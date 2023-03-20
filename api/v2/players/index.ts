dcf8bef71eabba501ef3de927100ec64e84af605
Micha Buchholz <micha@haus23.net>
2023-03-20 15:43:39 +0100

    feat: cache event handler

--- api/v2/players/index.ts
+++ api/v2/players/index.ts
@@ -1,7 +1,7 @@
 import { Player } from '@haus23/tipprunde-types';
 import { db, modelConverter } from '~/lib/firebase';
 
-export default eventHandler(async () => {
+export default cachedEventHandler(async () => {
   const snapshot = await db.collection('players').withConverter(modelConverter<Player>()).get();
   return snapshot.docs.map((doc) => doc.data());
 });
