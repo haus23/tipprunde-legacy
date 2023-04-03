bf1d22af39ddc52ed8fa88e43bdf37312930485f
Micha Buchholz <micha@haus23.net>
2023-04-03 10:49:20 +0200

    feat: add utility to extract and validate championship from request

---
+++ lib/util/get-championship-id.ts
@@ -0,0 +1,28 @@
+import { ChampionshipId } from '@haus23/tipprunde-types';
+import { H3Event } from 'h3';
+import { z } from 'zod';
+import { getChampionships } from '../query/aggregat/get-championships';
+
+export async function getChampionshipId(event: H3Event): Promise<string> {
+  const id = event.context.params?.id;
+  if (!id) {
+    throw new Error('No route param named id present.');
+  }
+
+  const parsedId = ChampionshipId.or(z.literal('current')).safeParse(id);
+  if (!parsedId.success) {
+    sendError(event, createError({ statusCode: 406, statusMessage: 'Bad championship id' }));
+    return 'error';
+  }
+
+  const championships = await getChampionships();
+
+  const championshipId =
+    parsedId.data === 'current' ? championships?.at(0)?.id : championships?.find((c) => c.id === parsedId.data)?.id;
+  if (!championshipId) {
+    sendError(event, createError({ statusCode: 404, statusMessage: 'Championship not found' }));
+    return 'error';
+  }
+
+  return championshipId;
+}
