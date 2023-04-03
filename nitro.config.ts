e392278968e189dd603415f5a77d38bc70c1968f
Micha Buchholz <micha@haus23.net>
2023-04-03 13:14:32 +0200

    feat: add cors headers for api queries

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,8 @@
 import { defineNitroConfig } from 'nitropack';
 
-export default defineNitroConfig({});
+export default defineNitroConfig({
+  routeRules: {
+    '/api/v1/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+    '/api/v2/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+  },
+});
