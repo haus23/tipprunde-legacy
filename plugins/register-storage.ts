e1cfe940a90c0dde5c5a3d981751fa5f72beb753
Micha Buchholz <micha@haus23.net>
2023-04-21 10:01:07 +0200

    refactor: align config reading.

--- plugins/register-storage.ts
+++ plugins/register-storage.ts
@@ -1,9 +1,10 @@
 import chained from '~/lib/unstorage/drivers/chained';
 import fs from 'unstorage/drivers/fs';
 import memory from 'unstorage/drivers/memory';
+import { config } from '~/lib/config';
 
 export default defineNitroPlugin(async () => {
   const storage = useStorage();
   await storage.unmount('cache');
-  storage.mount('cache', chained({ drivers: [memory(), fs({ base: process.env.STORAGE_BASE })] }));
+  storage.mount('cache', chained({ drivers: [memory(), fs({ base: config.unstorage.basePath })] }));
 });
