37791542eb85184a42407deb99559d94bfaa28f7
Micha Buchholz <micha@haus23.net>
2023-04-21 10:08:50 +0200

    feat: increase swr cache time to one hour

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,7 +2,7 @@ import { defineNitroConfig } from 'nitropack';
 
 export default defineNitroConfig({
   routeRules: {
-    '/api/v1/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
-    '/api/v2/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+    '/api/v1/**': { swr: 60 * 60, cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+    '/api/v2/**': { swr: 60 * 60, cors: true, headers: { 'access-control-allow-methods': 'GET' } },
   },
 });
