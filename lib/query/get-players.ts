140837cad07694cf5382defff4e67bfe70d060f7
Micha Buchholz <micha@haus23.net>
2023-04-21 11:02:39 +0200

    fix: cache players query as well

--- lib/query/get-players.ts
+++ lib/query/get-players.ts
@@ -1,7 +1,10 @@
 import { Player } from '@haus23/tipprunde-types';
 import { db, modelConverter } from '~/lib/firebase';
 
-export async function getPlayers() {
-  const snapshot = await db.collection('players').withConverter(modelConverter<Player>()).get();
-  return snapshot.docs.map((doc) => doc.data());
-}
+export const getPlayers = cachedFunction(
+  async () => {
+    const snapshot = await db.collection('players').withConverter(modelConverter<Player>()).get();
+    return snapshot.docs.map((doc) => doc.data());
+  },
+  { getKey: () => 'players', maxAge: 60 * 60 }
+);
