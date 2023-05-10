0e26d3a16416c2dd5feafdc3869220db807c152d
Micha Buchholz <micha@haus23.net>
2023-05-10 22:54:17 +0200

    refactor: add route level caching again. mainly for dev purposes. see #2

--- nitro.config.ts
+++ nitro.config.ts
@@ -2,6 +2,10 @@ import { defineNitroConfig } from 'nitropack/config';
 
 export default defineNitroConfig({
   routeRules: {
-    '/api/v1/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+    '/api/v1/**': {
+      cache: { maxAge: 60 * 60, swr: true },
+      cors: true,
+      headers: { 'access-control-allow-methods': 'GET' },
+    },
   },
 });
