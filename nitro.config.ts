f47e70418fa9f904e44c171614236bc3fbef755d
Micha Buchholz <micha@haus23.net>
2023-05-10 22:18:08 +0200

    fix: add cors headers again

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,3 +1,7 @@
-import { defineNitroConfig } from 'nitropack';
+import { defineNitroConfig } from 'nitropack/config';
 
-export default defineNitroConfig({});
+export default defineNitroConfig({
+  routeRules: {
+    '/api/v1/**': { cors: true, headers: { 'access-control-allow-methods': 'GET' } },
+  },
+});
