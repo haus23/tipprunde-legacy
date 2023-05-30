6455fce267502222c91c1e91f12b99ae3c97ba30
Micha Buchholz <micha@haus23.net>
2023-05-30 21:56:25 +0200

    fix: Update exports.

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,13 @@ export * from './model/aggregat/championship/player';
 
 // Entities (raw leaves)
 export * from './model/entity/account';
+export * from './model/entity/league';
 export * from './model/entity/team';
 export * from './model/entity/championship/match';
 export * from './model/entity/championship/member';
+export * from './model/entity/championship/round';
 export * from './model/entity/championship/tip';
 
 // Queries (collected data)
+export * from './model/queries/championship/matches';
 export * from './model/queries/championship/current-matches';
