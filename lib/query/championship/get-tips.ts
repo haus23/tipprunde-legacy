354ecca72f14e2f74f810d65beb551448ee2bc9c
Micha Buchholz <micha@haus23.net>
2023-05-31 22:18:57 +0200

    feat: query tips by player

--- lib/query/championship/get-tips.ts
+++ lib/query/championship/get-tips.ts
@@ -10,3 +10,14 @@ export async function getTips(championshipId: string) {
     .get();
   return snapshot.docs.map((doc) => doc.data());
 }
+
+export async function getTipsByPlayer(championshipId: string, playerId: string) {
+  console.info(`[${new Date().toLocaleString()}] Querying tips ${championshipId}`);
+
+  const snapshot = await db
+    .collection(`championships/${championshipId}/tips`)
+    .where('playerId', '==', playerId)
+    .withConverter(modelConverter<Tip>())
+    .get();
+  return snapshot.docs.map((doc) => doc.data());
+}
