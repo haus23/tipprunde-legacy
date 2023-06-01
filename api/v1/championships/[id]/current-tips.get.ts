ba3d67c7078221dcf80962009d378553f4f7d530
Micha Buchholz <micha@haus23.net>
2023-06-01 23:26:39 +0200

    refactor: Align with upgraded type. Rename route.

--- api/v1/championships/[id]/current-tips.get.ts
+++ api/v1/championships/[id]/current-tips.get.ts
@@ -1,4 +1,4 @@
-import { CurrentMatches } from '@haus23/tipprunde-types';
+import { CurrentTips } from '@haus23/tipprunde-types';
 import { getMatches } from '~/lib/query/championship/get-matches';
 import { getTips } from '~/lib/query/championship/get-tips';
 import { getTeams } from '~/lib/query/get-teams';
@@ -11,29 +11,19 @@ export default defineEventHandler(async (event) => {
   const tips = (await getTips(championshipId)) || [];
   const teams = (await getTeams()) || [];
 
-  // sort matches by date
   matches.sort((a, b) => a.date.localeCompare(b.date));
-  // find last match with result
   const lastMatchIx = matches.findLastIndex((m) => m.result);
 
   let currentSliceStart = Math.min(Math.max(0, lastMatchIx - 1), matches.length - 4);
   const currentSlice = matches.slice(currentSliceStart, currentSliceStart + 4);
 
-  // Collect teams in currentSlice
-  const currentTeamIds = new Set(currentSlice.map((m) => [m.hometeamId, m.awayteamId]).flat());
-  const currentTeams = new Map(teams.filter((t) => currentTeamIds.has(t.id)).map((t) => [t.id, t]));
+  const currentTips = currentSlice.map((match) => {
+    const tipsPerMatch = new Map(tips.filter((t) => t.matchId === match.id).map((t) => [t.playerId, t]));
+    return {
+      matchId: match.id,
+      tips: Object.fromEntries(tipsPerMatch),
+    };
+  }) satisfies CurrentTips;
 
-  // add teams and the tips by players
-  const currentMatches = {
-    matches: currentSlice.map((match) => {
-      const tipsPerMatch = new Map(tips.filter((t) => t.matchId === match.id).map((t) => [t.playerId, t]));
-      return {
-        ...match,
-        tips: Object.fromEntries(tipsPerMatch),
-      };
-    }),
-    teams: Object.fromEntries(currentTeams),
-  } satisfies CurrentMatches;
-
-  return currentMatches;
+  return currentTips;
 });
