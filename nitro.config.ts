2a225f8bedcd6f655b6a8b3c6cfc6469cb257ca8
Micha Buchholz <micha@haus23.net>
2023-06-06 20:33:54 +0200

    chore(cache): Enable http caching for ten minutes.

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,7 +3,7 @@ import { defineNitroConfig } from 'nitropack/config';
 export default defineNitroConfig({
   routeRules: {
     '/api/v1/**': {
-      // cache: { maxAge: 60 * 60, swr: true },
+      cache: { maxAge: 10 * 60, swr: true },
       cors: true,
       headers: { 'access-control-allow-methods': 'GET' },
     },
