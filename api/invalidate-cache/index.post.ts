fd10ab31051162b2d090699dad214d237d8da77a
Micha Buchholz <micha@haus23.net>
2023-10-03 21:52:40 +0200

    feat: Implement cache invalidation route.

---
+++ api/invalidate-cache/index.post.ts
@@ -0,0 +1,49 @@
+import { ChampionshipId } from '@haus23/tipprunde-types';
+import { z } from 'zod';
+
+const CacheKeys = z.object({
+  routes: z.boolean(),
+  resources: z.array(
+    z.union([z.literal('accounts'), z.literal('championships'), z.literal('leagues'), z.literal('teams')])
+  ),
+  standings: ChampionshipId.optional(),
+});
+
+type CacheKeys = z.infer<typeof CacheKeys>;
+
+export default defineEventHandler(async (event) => {
+  const body = await readBody(event);
+  const cacheKeys = CacheKeys.parse(body);
+
+  const clearedCaches = new Array<string>();
+
+  const storage = useStorage('cache');
+  const keys = await storage.getKeys();
+
+  if (cacheKeys.routes) {
+    keys
+      .filter((k) => k.startsWith('nitro:routes:'))
+      .forEach(async (k) => {
+        await storage.removeItem(k, true);
+        clearedCaches.push(k);
+      });
+  }
+
+  cacheKeys.resources.forEach(async (resourceKey) => {
+    const cacheItemKey = keys.find((k) => k.includes(`:${resourceKey}:`));
+    if (cacheItemKey) {
+      await storage.removeItem(cacheItemKey, true);
+      clearedCaches.push(cacheItemKey);
+    }
+  });
+
+  if (cacheKeys.standings) {
+    const cacheItemKey = keys.find((k) => k.includes(`:${cacheKeys.standings}.json`));
+    if (cacheItemKey) {
+      await storage.removeItem(cacheItemKey, true);
+      clearedCaches.push(cacheItemKey);
+    }
+  }
+
+  return { cleared: clearedCaches };
+});
