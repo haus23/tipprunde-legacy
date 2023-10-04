ecaf36bd7fdd707249eadbeee44fad3aa6a6d9e0
Micha Buchholz <micha@haus23.net>
2023-10-04 12:59:36 +0200

    feat: Add rules type

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ export * from './model/aggregat/championship/player';
 // Entities (raw leaves)
 export * from './model/entity/account';
 export * from './model/entity/league';
+export * from './model/entity/rule';
 export * from './model/entity/team';
 export * from './model/entity/championship/match';
 export * from './model/entity/championship/member';
