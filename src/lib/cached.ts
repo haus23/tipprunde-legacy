bfa986641146406062cb7c2ddaec1dfb76e1e95b
Micha Buchholz <micha@haus23.net>
2025-01-03 09:33:37 +0100

    refactor: Make maxAge setable via function.

--- src/lib/cached.ts
+++ src/lib/cached.ts
@@ -4,7 +4,7 @@ import { storage } from './storage.ts';
 export type CacheOptions<FnArgsT extends unknown[]> = {
   name: string;
   getKey: (...args: FnArgsT) => string;
-  maxAge?: number;
+  maxAge?: number | ((...args: FnArgsT) => number);
 };
 
 export type CachedEntry<T> = {
@@ -16,9 +16,11 @@ export function cachedFunction<T, FnArgsT extends unknown[]>(
   fn: (...args: FnArgsT) => T | Promise<T>,
   { getKey, ...options }: CacheOptions<FnArgsT>,
 ) {
-  const maxAge = options.maxAge ?? env.MAX_AGE;
-
-  async function get(key: string, resolver: () => T | Promise<T>) {
+  async function get(
+    key: string,
+    maxAge: number,
+    resolver: () => T | Promise<T>,
+  ) {
     const cacheKey = `${options.name}:${key}.json`;
     const now = Date.now();
 
@@ -33,7 +35,11 @@ export function cachedFunction<T, FnArgsT extends unknown[]>(
   }
 
   return async (...args: FnArgsT) => {
-    const entry = await get(getKey(...args), () => fn(...args));
+    const maxAge =
+      typeof options.maxAge === 'function'
+        ? options.maxAge(...args)
+        : (options.maxAge ?? env.MAX_AGE);
+    const entry = await get(getKey(...args), maxAge, () => fn(...args));
     const value = entry.value;
     return value;
   };
