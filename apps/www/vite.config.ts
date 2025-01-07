5a0943268face8435df12f83e64dd9fc43d1620a
Micha Buchholz <micha@haus23.net>
2025-01-07 11:30:22 +0100

    fix: Add sourcemaps.

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  build: {
+    sourcemap: true,
+  },
   plugins: [react(), tsconfigPaths()],
 });
