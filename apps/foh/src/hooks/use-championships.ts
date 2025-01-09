576eb5ac26459915e39d88950a65d93cdf739188
Micha Buchholz <micha@haus23.net>
2025-01-09 16:29:21 +0100

    migrate: Remove old foh implementation.

--- apps/foh/src/hooks/use-championships.ts
+++
@@ -1,6 +0,0 @@
-import { useRouteLoaderData } from 'react-router-dom';
-
-export function useChampionships() {
-  const { championships } = useRouteLoaderData('app');
-  return championships;
-}
--- apps/foh/src/utils/merge-element-props.ts
+++
@@ -1,4 +0,0 @@
-export type MergeElementProps<
-  T extends React.ElementType,
-  P extends object = Record<string, never>
-> = Omit<React.ComponentPropsWithRef<T>, keyof P> & P;
