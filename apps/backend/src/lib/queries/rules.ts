000a2c30fe9a78adaf636ee726b7730e11d6f69d
Micha Buchholz <micha@haus23.net>
2025-01-09 15:11:36 +0100

    refactor: Adapt model change.

--- apps/backend/src/lib/queries/rules.ts
+++ apps/backend/src/lib/queries/rules.ts
@@ -1,4 +1,4 @@
-import type { RuleInput } from '@haus23/tipprunde-model';
+import type { RulesetInput } from '@haus23/tipprunde-model';
 
 import { cachedFunction } from '../cached.ts';
 import { db, modelConverter } from '../firebase/index.ts';
@@ -9,7 +9,7 @@ export const getRules = cachedFunction(
 
     const snapshot = await db
       .collection('rules')
-      .withConverter(modelConverter<RuleInput>())
+      .withConverter(modelConverter<RulesetInput>())
       .get();
     return snapshot.docs.map((doc) => doc.data());
   },
