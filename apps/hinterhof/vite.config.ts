5f4a2e78702477b8b0843c616df2803a56949e74
Micha Buchholz <micha@haus23.net>
2025-01-21 16:29:45 +0100

    chore: Switch to vite plugin. And remove update tool.

--- apps/hinterhof/vite.config.ts
+++ apps/hinterhof/vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   build: {
     rollupOptions: {
       output: {
