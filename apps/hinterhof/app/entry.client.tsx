7cbb3ade0c4e9d3e6d40e2bf2c22f2a10ba8a7f4
Micha Buchholz <micha@haus23.net>
2025-01-21 20:03:31 +0100

    fix: Add styles.

--- apps/hinterhof/app/entry.client.tsx
+++ apps/hinterhof/app/entry.client.tsx
@@ -2,6 +2,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 
 import App from './app';
+import './styles/index.css';
 
 const rootContainer = document.getElementById('root');
 
