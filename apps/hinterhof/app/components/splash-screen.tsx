0d93ec4c4ae84ee64c48d915d59ca9cbb59e9c20
Micha Buchholz <micha@haus23.net>
2025-01-22 13:14:01 +0100

    feat: Add splash screen.

---
+++ apps/hinterhof/app/components/splash-screen.tsx
@@ -0,0 +1,20 @@
+import { tv } from 'tailwind-variants';
+
+import { Logo } from './logo';
+
+const styles = tv({ base: 'min-h-svh grid place-content-center' });
+
+namespace SplashScreen {
+  export interface Props extends React.ComponentProps<'div'> {}
+}
+
+export function SplashScreen({ className, ...props }: SplashScreen.Props) {
+  return (
+    <div className={styles({ className })} {...props}>
+      <Logo className="size-64 text-primary/80 md:size-96" />
+      <span className="block text-center font-medium text-4xl tracking-wide md:text-5xl">
+        runde.tips
+      </span>
+    </div>
+  );
+}
