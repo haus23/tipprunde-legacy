1386880ebd4ff982a51e8e9047bc00e67ef28df3
Micha Buchholz <micha@haus23.net>
2025-01-22 08:59:49 +0100

    feat: Design the login route.

--- apps/hinterhof/app/routes/login.tsx
+++ apps/hinterhof/app/routes/login.tsx
@@ -1,10 +1,39 @@
-import { Button } from "../components/ui";
+import { Logo } from '../components/logo';
+import {
+  Button,
+  Card,
+  Container,
+  Form,
+  Heading,
+  TextField,
+} from '../components/ui';
 
 export default function LoginRoute() {
   return (
-    <div>
-      <h2>Anmeldung</h2>
-      <Button>Anmelden</Button>
-    </div>
+    <Container
+      intent="constrained"
+      className="grid min-h-svh place-items-center"
+    >
+      <div className="flex w-full flex-col gap-8 sm:flex-row md:flex-col">
+        <div className="flex flex-col self-center">
+          <Logo className="mx-auto size-32 text-secondary-fg" />
+          <span className="mx-auto font-medium text-2xl">Hinterhof</span>
+        </div>
+        <Card className="mx-2 grow sm:mx-0 md:min-w-md md:self-center">
+          <Card.Header>
+            <Heading>Anmeldung</Heading>
+          </Card.Header>
+          <Card.Content>
+            <Form className="flex flex-col gap-y-2">
+              <TextField label="Email *" />
+              <TextField label="Passwort *" type="password" isRevealable />
+            </Form>
+          </Card.Content>
+          <Card.Footer>
+            <Button className="w-full">Anmelden</Button>
+          </Card.Footer>
+        </Card>
+      </div>
+    </Container>
   );
 }
