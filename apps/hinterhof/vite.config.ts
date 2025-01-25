05b4682deddab8b588f1a205897f3ce068ce3398
Micha Buchholz <micha@haus23.net>
2025-01-25 18:58:29 +0100

    refactor: Add shadcn deps and make the styles switch.

--- apps/hinterhof/vite.config.ts
+++ apps/hinterhof/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     rollupOptions: {
       output: {
         manualChunks: {
-          lib: ['lib'],
+          firebase: ['firebase'],
         },
       },
     },
