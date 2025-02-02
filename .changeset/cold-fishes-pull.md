481b22a91dc6897a0ab35d02ff3324ffc8a6fa70
Micha Buchholz <micha@haus23.net>
2025-02-02 20:42:42 +0100

    refactor: Loose id schema. League IDs could be just two letters long.

---
+++ .changeset/cold-fishes-pull.md
@@ -0,0 +1,5 @@
+---
+"@haus23/tipprunde-model": patch
+---
+
+Loose id schema.
--- packages/model/src/model/id.ts
+++ packages/model/src/model/id.ts
@@ -3,7 +3,7 @@ import * as v from 'valibot';
 export const IdSchema = v.pipe(
   v.string(),
   v.nonEmpty('Kann nicht leer sein.'),
-  v.minLength(3, 'Eine ID muss mindestens drei Zeichen haben'),
-  v.regex(/^\w[-\w]+\w$/, 'Ungültige ID.'),
+  v.minLength(2, 'Eine ID muss mindestens zwei Zeichen haben'),
+  v.regex(/^\w[-\w]*\w$/, 'Ungültige ID.'),
 );
 export const OptionalIdSchema = v.nullish(v.string(), '');
