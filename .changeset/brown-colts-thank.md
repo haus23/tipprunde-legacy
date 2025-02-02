85650325df0011ecad4d76b1b81185726c782178
Micha Buchholz <micha@haus23.net>
2025-02-02 19:27:30 +0100

    fix: Accept null values as well.

---
+++ .changeset/brown-colts-thank.md
@@ -0,0 +1,5 @@
+---
+"@haus23/tipprunde-model": minor
+---
+
+Accept null values as well.
--- packages/model/src/model/id.ts
+++ packages/model/src/model/id.ts
@@ -1,4 +1,4 @@
 import * as v from 'valibot';
 
 export const IdSchema = v.pipe(v.string(), v.nonEmpty());
-export const OptionalIdSchema = v.optional(v.string(), '');
+export const OptionalIdSchema = v.nullish(v.string(), '');
