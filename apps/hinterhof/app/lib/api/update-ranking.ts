c77100faa36d254debb648ce2bed98def01c1c34
Micha Buchholz <micha@haus23.net>
2025-02-26 21:40:36 +0100

    feat: (Re-) calculate ranking.

--- apps/hinterhof/app/lib/api/update-ranking.ts
+++ apps/hinterhof/app/lib/api/update-ranking.ts
@@ -1,3 +1,44 @@
+import { currentChampionshipAtom } from '@/utils/state/current-championship/championship';
+import { playersAtom } from '@/utils/state/current-championship/players';
+import { tipsAtom } from '@/utils/state/current-championship/tips';
+import { store } from '@/utils/store';
+import type { Player } from '@haus23/tipprunde-model';
+import { patchEntity } from '../firebase/repository';
+
 export async function updateRanking() {
-  console.log('Update ranking');
+  const championship = store.get(currentChampionshipAtom);
+  const { players } = store.get(playersAtom(championship.id));
+  const { tips } = store.get(tipsAtom(championship.id));
+
+  for (const p of players) {
+    const points = tips
+      .filter((t) => t.playerId === p.id)
+      .reduce((sum, t) => sum + t.points, 0);
+    p.points = points;
+    p.totalPoints = p.extraPoints + p.points;
+  }
+
+  let previousRank = 0;
+  let rank = 0;
+  let previousPoints = -1;
+  for (const p of players.toSorted((a, b) => b.totalPoints - a.totalPoints)) {
+    ++rank;
+    if (p.totalPoints !== previousPoints) {
+      p.rank = rank;
+      previousRank = rank;
+      previousPoints = p.totalPoints;
+    } else {
+      p.rank = previousRank;
+    }
+  }
+
+  await Promise.all(
+    players.map((p) =>
+      patchEntity<Player>(`championships/${championship.id}/players`, p.id, {
+        points: p.points,
+        totalPoints: p.totalPoints,
+        rank: p.rank,
+      }),
+    ),
+  );
 }
