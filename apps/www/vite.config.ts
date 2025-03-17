317348e6bfe8b6edddfd6a96f2e6c78e110a63a9
Micha Buchholz <micha@haus23.net>
2025-03-17 12:30:03 +0100

    refactor: Switch to vite plugin. Dismiss postcss. Finally resolves #24

--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
@@ -7,5 +8,5 @@ export default defineConfig({
   build: {
     sourcemap: true,
   },
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
