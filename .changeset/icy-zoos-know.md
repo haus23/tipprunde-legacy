0fca95bafb542c0659b97c77959e933a027499df
Micha Buchholz <micha@haus23.net>
2025-03-17 12:44:37 +0100

    fix: Create extra chunk for react router to reduce main bundle size.

---
+++ .changeset/icy-zoos-know.md
@@ -0,0 +1,5 @@
+---
+"@haus23/tipprunde-www": patch
+---
+
+Create extra chunk for react router to reduce main bundle size.
--- apps/www/vite.config.ts
+++ apps/www/vite.config.ts
@@ -7,6 +7,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   build: {
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          router: ['react-router'],
+        },
+      },
+    },
   },
   plugins: [react(), tailwindcss(), tsconfigPaths()],
 });
