a1c5db98c2fcea9970d32f7af7871a142e564fab
Micha Buchholz <micha@haus23.net>
2025-03-19 09:55:31 +0100

    feat: Re-add logo and splash-screen.

--- apps/www/app/components/splash-screen.tsx
+++ apps/www/app/components/splash-screen.tsx
@@ -2,7 +2,7 @@ import { tv } from 'tailwind-variants';
 
 import { Logo } from './logo';
 
-const styles = tv({ base: 'min-h-svh grid place-content-center' });
+const styles = tv({ base: 'grid min-h-svh place-content-center' });
 
 namespace SplashScreen {
   export interface Props extends React.ComponentProps<'div'> {}
@@ -11,8 +11,8 @@ namespace SplashScreen {
 export function SplashScreen({ className, ...props }: SplashScreen.Props) {
   return (
     <div className={styles({ className })} {...props}>
-      <Logo className="size-64 md:size-80 text-primary-foreground" />
-      <span className="block text-center text-4xl tracking-wide md:text-5xl font-medium">
+      <Logo className="size-64 text-accent-12 md:size-80" />
+      <span className="block text-center font-medium text-4xl tracking-wide md:text-5xl">
         runde.tips
       </span>
     </div>
