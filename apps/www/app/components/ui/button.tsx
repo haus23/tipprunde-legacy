0b5335d9d76242d2fe705ab738c599f78a0e328a
Micha Buchholz <micha@haus23.net>
2025-03-19 13:53:19 +0100

    feat: Re-add lean button - based on rac button.

--- apps/www/app/components/ui/button.tsx
+++ apps/www/app/components/ui/button.tsx
@@ -1,29 +1,34 @@
+import { type ButtonProps, Button as _Button } from 'react-aria-components';
 import { type VariantProps, tv } from 'tailwind-variants';
 
-import { focusStyles } from '../focus-styles';
+import { focusStyles } from './_styles';
 
 const buttonStyles = tv({
   extend: focusStyles,
   base: 'inline-flex items-center justify-center font-medium text-sm transition-colors',
   variants: {
     variant: {
-      default:
+      primary:
         'border bg-accent-9 px-4 py-2 text-white transition-transform hover:bg-accent-10 active:scale-[0.98]',
-      toolbar: 'p-2 hover:bg-accent-4',
-      tableHead: '',
+      secondary: '',
+      ghost: 'p-2 hover:bg-accent-4',
     },
   },
   defaultVariants: {
-    variant: 'default',
+    variant: 'primary',
   },
 });
 
 namespace Button {
   export interface Props
-    extends React.ComponentProps<'button'>,
-      VariantProps<typeof buttonStyles> {}
+    extends ButtonProps,
+      VariantProps<typeof buttonStyles> {
+    className?: string;
+  }
 }
 
 export function Button({ className, variant, ...props }: Button.Props) {
-  return <button className={buttonStyles({ variant, className })} {...props} />;
+  return (
+    <_Button className={buttonStyles({ variant, className })} {...props} />
+  );
 }
