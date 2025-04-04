916f833ac68489892fdb408259c694b205f4fdca
Micha Buchholz <micha@haus23.net>
2025-04-04 09:39:32 +0200

    refactor: Re-add date-format function.

--- apps/www/app/utils/misc.ts
+++ apps/www/app/utils/misc.ts
@@ -2,6 +2,11 @@ import { useEffect, useState } from 'react';
 
 const MOBILE_BREAKPOINT = 768;
 
+/**
+ * Hook watching my mobile breakpoint
+ *
+ * @returns true if screen width is mobile
+ */
 export function useIsMobile() {
   const [isMobile, setIsMobile] = useState<boolean | undefined>(undefined);
 
@@ -16,4 +21,35 @@ export function useIsMobile() {
   }, []);
 
   return !!isMobile;
+}
+
+/**
+ * Formats a date value
+ *
+ * @param dateStr Date value as string
+ * @param options Determine short date format options
+ * @returns Formatted date
+ */
+export function formatDate(
+  dateStr: string,
+  options?: { short?: boolean; shortIfCurrent?: boolean },
+) {
+  let { short, shortIfCurrent } = options || {
+    short: false,
+    shortIfCurrent: false,
+  };
+
+  if (!dateStr) return '';
+
+  const date = new Date(dateStr);
+
+  if (shortIfCurrent && new Date().getFullYear() === date.getFullYear()) {
+    short = true;
+  }
+
+  return date.toLocaleDateString('de-DE', {
+    day: '2-digit',
+    month: '2-digit',
+    year: short ? undefined : '2-digit',
+  });
 }
