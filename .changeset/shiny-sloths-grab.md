474962845950da5cc393105a050dfb05a3a48082
Micha Buchholz <micha@haus23.net>
2025-04-05 18:59:18 +0200

    feat: Add match selector to match route.

---
+++ .changeset/shiny-sloths-grab.md
@@ -0,0 +1,5 @@
+---
+"@haus23/tipprunde-www": patch
+---
+
+Add match selector to match route.
--- apps/www/app/routes/spiele_/route.tsx
+++ apps/www/app/routes/spiele_/route.tsx
@@ -1,9 +1,69 @@
-import { createFileRoute } from '@tanstack/react-router';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import {
+  createFileRoute,
+  useLoaderData,
+  useNavigate,
+} from '@tanstack/react-router';
+import { Collection, type Key } from 'react-aria-components';
+import * as v from 'valibot';
+
+import { Header } from '#/components/ui/header';
+import { ListBoxItem, ListBoxSection } from '#/components/ui/listbox';
+import { Select } from '#/components/ui/select';
+import { matchTipsQuery, matchesQuery } from '#/utils/queries';
 
 export const Route = createFileRoute('/spiele_')({
+  validateSearch: v.object({
+    nr: v.optional(v.number()),
+  }),
+  loaderDeps: ({ search: { nr } }) => ({ nr }),
+  loader: async ({ deps: { nr }, context: { queryClient, championship } }) => {
+    const matches = await queryClient.ensureQueryData(
+      matchesQuery(championship.id),
+    );
+    const match =
+      matches.matches.find((m) => m.nr === nr) || matches.matches[0];
+
+    queryClient.prefetchQuery(matchTipsQuery(championship.id, nr ?? null));
+    return { match };
+  },
   component: MatchesComponent,
 });
 
 function MatchesComponent() {
-  return <h1 className="text-2xl">Spiele</h1>;
+  const { championship } = useLoaderData({ from: '__root__' });
+  const navigate = useNavigate({ from: Route.fullPath });
+
+  const { match } = Route.useLoaderData();
+  function selectMatch(key: Key) {
+    navigate({ search: (prev) => ({ ...prev, nr: Number(key) }) });
+  }
+
+  const {
+    data: { matches, rounds, teams },
+  } = useSuspenseQuery(matchesQuery(championship.id));
+
+  return (
+    <div>
+      <Select
+        label="Spiel"
+        defaultSelectedKey={match.nr}
+        onSelectionChange={selectMatch}
+        items={rounds}
+      >
+        {(r) => (
+          <ListBoxSection id={r.id}>
+            <Header>Runde {r.nr}</Header>
+            <Collection items={matches.filter((m) => m.roundId === r.id)}>
+              {(m) => (
+                <ListBoxItem id={m.nr}>
+                  {`${teams[m.hometeamId]?.shortname || 'TBA'} - ${teams[m.awayteamId]?.shortname || 'TBA'}`}
+                </ListBoxItem>
+              )}
+            </Collection>
+          </ListBoxSection>
+        )}
+      </Select>
+    </div>
+  );
 }
