b85770b7bce698bf464ec26484b879266ed0d4bb
Micha Buchholz <micha@haus23.net>
2025-04-10 09:33:52 +0200

    feat: Initial displaying match tips.

--- apps/www/app/routes/spiele_/route.tsx
+++ apps/www/app/routes/spiele_/route.tsx
@@ -1,17 +1,23 @@
+import type { Tip } from '@haus23/tipprunde-model';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import {
   createFileRoute,
   useLoaderData,
   useNavigate,
 } from '@tanstack/react-router';
+import { type ColumnDef, createColumnHelper } from '@tanstack/react-table';
+import { useMemo } from 'react';
 import { Collection, type Key } from 'react-aria-components';
 import * as v from 'valibot';
 
 import { CheckIcon } from 'lucide-react';
+import { DataTable } from '#/components/ui/data-table';
 import { Header } from '#/components/ui/header';
 import { ListBoxItem, ListBoxSection } from '#/components/ui/listbox';
 import { Select } from '#/components/ui/select';
-import { matchTipsQuery, matchesQuery } from '#/utils/queries';
+import { matchTipsQuery, matchesQuery, playersQuery } from '#/utils/queries';
+
+const columnHelper = createColumnHelper<Tip>();
 
 export const Route = createFileRoute('/spiele_')({
   validateSearch: v.object({
@@ -22,6 +28,7 @@ export const Route = createFileRoute('/spiele_')({
     const matches = await queryClient.ensureQueryData(
       matchesQuery(championship.id),
     );
+    // TODO: default match is last match in current championship
     const match =
       matches.matches.find((m) => m.nr === nr) || matches.matches[0];
 
@@ -40,40 +47,74 @@ function MatchesComponent() {
     navigate({ search: (prev) => ({ ...prev, nr: Number(key) }) });
   }
 
+  const { data: players } = useSuspenseQuery(playersQuery(championship.id));
+  const {
+    data: { tips },
+  } = useSuspenseQuery(matchTipsQuery(championship.id, match.nr));
+
   const {
     data: { matches, rounds, teams },
   } = useSuspenseQuery(matchesQuery(championship.id));
 
+  const matchTips = useMemo(
+    () => players.map((p) => tips[p.id] || { playerId: p.id }),
+    [players, tips],
+  );
+
+  const columns = useMemo(
+    () =>
+      [
+        columnHelper.accessor('playerId', {
+          header: 'Spieler',
+          cell: (info) =>
+            players.find((p) => p.id === info.getValue())?.account.name,
+        }),
+        columnHelper.accessor('tip', {
+          header: 'Tipp',
+        }),
+        columnHelper.accessor('points', {
+          header: 'Punkte',
+        }),
+      ] as ColumnDef<Tip>[],
+    [players],
+  );
+
   return (
     <div>
-      <Select
-        aria-label="Spielauswahl"
-        defaultSelectedKey={match.nr}
-        onSelectionChange={selectMatch}
-        items={rounds}
-      >
-        {(r) => (
-          <ListBoxSection id={r.id}>
-            <Header className="p-1 text-gray-11/75 text-sm">
-              Runde {r.nr}
-            </Header>
-            <Collection items={matches.filter((m) => m.roundId === r.id)}>
-              {(m) => (
-                <ListBoxItem id={m.nr}>
-                  {({ isSelected }) => (
-                    <>
-                      <span>
-                        {`${teams[m.hometeamId]?.shortname || 'TBA'} - ${teams[m.awayteamId]?.shortname || 'TBA'}`}
-                      </span>
-                      {isSelected && <CheckIcon className="size-5" />}
-                    </>
-                  )}
-                </ListBoxItem>
-              )}
-            </Collection>
-          </ListBoxSection>
-        )}
-      </Select>
+      <div>
+        <Select
+          aria-label="Spielauswahl"
+          defaultSelectedKey={match.nr}
+          onSelectionChange={selectMatch}
+          items={rounds}
+        >
+          {(r) => (
+            <ListBoxSection id={r.id}>
+              <Header className="px-1 pt-4 text-gray-11/85 text-sm">
+                Runde {r.nr}
+              </Header>
+              <Collection items={matches.filter((m) => m.roundId === r.id)}>
+                {(m) => {
+                  const matchStr = `${teams[m.hometeamId]?.shortname || 'TBA'} - ${teams[m.awayteamId]?.shortname || 'TBA'}`;
+                  return (
+                    <ListBoxItem id={m.nr} textValue={matchStr}>
+                      {({ isSelected }) => (
+                        <>
+                          <span>{matchStr}</span>
+                          {isSelected && <CheckIcon className="size-5" />}
+                        </>
+                      )}
+                    </ListBoxItem>
+                  );
+                }}
+              </Collection>
+            </ListBoxSection>
+          )}
+        </Select>
+      </div>
+      <div>
+        <DataTable columns={columns} data={matchTips} />
+      </div>
     </div>
   );
 }
