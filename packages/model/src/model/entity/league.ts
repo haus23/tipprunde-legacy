552cb48049d1af5af07d5440cfe42191322dd045
Micha Buchholz <micha@haus23.net>
2025-06-05 21:57:57 +0200

    feat: Add updatedAt property to league schema.

--- packages/model/src/model/entity/league.ts
+++ packages/model/src/model/entity/league.ts
@@ -1,6 +1,7 @@
 import * as v from 'valibot';
 
 import { IdSchema } from '../id';
+import { UpdatedAtSchema } from '../updatedAt';
 
 // Firebase collection path: /leagues
 
@@ -8,6 +9,7 @@ export const LeagueSchema = v.object({
   id: IdSchema,
   name: v.pipe(v.string(), v.nonEmpty()),
   shortname: v.pipe(v.string(), v.nonEmpty()),
+  updated_at: v.optional(UpdatedAtSchema),
 });
 
 export type LeagueInput = v.InferInput<typeof LeagueSchema>;
